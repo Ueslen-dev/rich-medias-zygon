0256551c867ba04a3f98b35de4184a5de7e89403
Ueslen Santana <ueslen.santana@outlook.com>
2022-12-21 21:30:16 -0300

    create rich media countdown

--- contador/assets/js/main.js
+++ contador/assets/js/main.js
@@ -1,84 +1,70 @@
-	// #CD_CODE
-	var i0 = document.getElementById('count0');
-	var i1 = document.getElementById('count1');	  	  	
-	var s = "assets/img/n/";
-	var ext = ".png";
-	var days;
-	var hours;
-	var minutes;
-	var seconds;
-	var lbl_f;
-	var lbl_d;
-	var lbl_h;
-	var isExpanded = false;	
-	var countDownDate = new Date("Oct 2, 2017 10:30:00").getTime();	
-	
-	var x = setInterval(function() {
-		var now = new Date().getTime();
-		var distance = countDownDate - now;
+const months = {
+	january: 'Jan',
+	february: 'Feb',
+	march: 'Mar',
+	april: 'Apr',	
+	may: 'May',
+	june: 'Jun',
+	july: 'Jul',
+	august: 'Aug',
+	setember: 'Set',
+	october: 'Oct',
+	november: 'Nov',
+	december: 'Dec'
+}
 
-		days = Math.floor(distance / (1000 * 60 * 60 * 24));
-		hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-		minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-		seconds = Math.floor((distance % (1000 * 60)) / 1000);
+const countDownDate = new Date(`${months.december} 31, 2022 21:28:00`).getTime();
 
-		if(days == 1){
-			lbl_f = "FALTA";
-			lbl_d = "DIA";
-		}else{
-			lbl_f="FALTAM";
-			lbl_d = "DIAS";
-		}
+const countdownExecute = setInterval(() => {
+  const currentDate = new Date().getTime();
+  const distance = countDownDate - currentDate;
+    
+  const daysMiliseconds = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hoursMiliseconds = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutesMiliseconds = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const secondsMiliseconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-		if(hours == 1){
-			lbl_h = "hora";
-		}else{
-			lbl_h = "horas";
-		}
+  const numberOfDays = document.querySelector('#days')
+  const numberOfHours = document.querySelector('#hours')
+  const numberOfMinutes = document.querySelector('#minutes')
+  const numberOfSeconds = document.querySelector('#seconds')
 
-			if (distance < 0) {//END
+  const countdownLabel = document.querySelector('#countdownLabel')
+  const daysLabel = document.querySelector('#daysLabel')
+  const hoursLabel = document.querySelector('#hoursLabel')
+  const minutesLabel = document.querySelector('#minutesLabel')
+  const secondsLabel = document.querySelector('#secondsLabel')
 
-				clearInterval(x);
-				var ff = ("0" + days).slice(-2);	
+  numberOfDays.innerHTML = daysMiliseconds
+  numberOfHours.innerHTML = hoursMiliseconds
+  numberOfMinutes.innerHTML = minutesMiliseconds
+  numberOfSeconds.innerHTML = secondsMiliseconds
 
-				hours = ("0" + hours).slice(-2);
-				minutes = ("0" + minutes).slice(-2);
-				seconds = ("0" + seconds).slice(-2);  
+  if(daysMiliseconds <= 1) {
+	countdownLabel.innerHTML = 'FALTA'
+	daysLabel.innerHTML = 'Dia'
+  }
+  if(hoursMiliseconds <= 1) {
+	hoursLabel.innerHTML = 'Hora'
+  }
+  if(minutesMiliseconds <= 1) {
+	minutesLabel.innerHTML = 'Minuto'
+  }
+  if(secondsMiliseconds <= 1) {
+	secondsLabel.innerHTML = 'Segundo'
+  }
 
-				s0 = s + 0 + ext;
-				s1 = s + 0 + ext;
-				i0.src = s0;
-				i1.src = s1;
+  if (distance <= 0) {
+    clearInterval(countdownExecute);
 
-				document.getElementById("hr").innerHTML = '00';
-				document.getElementById("min").innerHTML = '00';
-				document.getElementById("sec").innerHTML = '00';
-				document.getElementById("txt-f").innerHTML = 'FALTAM';
-				document.getElementById("txt-d-s").innerHTML = 'DIAS';
-				document.getElementById("hora-s").innerHTML = 'horas';
+	countdownLabel.innerHTML = 'ACABOU!'
+	numberOfDays.innerHTML = '00'
+	numberOfHours.innerHTML = '00'
+	numberOfMinutes.innerHTML = '00'
+	numberOfSeconds.innerHTML = '00'
+  }
+}, 1000);
 
-			}else{
-				var ff = ("0" + days).slice(-2);
-
-				hours = ("0" + hours).slice(-2);
-				minutes = ("0" + minutes).slice(-2);
-				seconds = ("0" + seconds).slice(-2);
-
-				s0 = s + ff.charAt(0) + ext;
-				s1 = s + ff.charAt(1) + ext;
-				i0.src = s0;
-				i1.src = s1;				
-
-				document.getElementById("hr").innerHTML = hours;
-				document.getElementById("min").innerHTML = minutes;
-				document.getElementById("sec").innerHTML = seconds;
-				document.getElementById("txt-f").innerHTML = lbl_f;
-				document.getElementById("txt-d-s").innerHTML = lbl_d;
-				document.getElementById("hora-s").innerHTML = lbl_h;
-			}		
-		}, 350);
-
-	function expand(){
-		document.getElementById('expanded').style.display = 'block';
-		isExpanded = true;
-	}	
+const callToAction = () => {
+	return window.open('https://zygon.digital/')
+}
